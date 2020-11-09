1c89c0389fcda51a05fdc6c44b57b02fa5d22024
Rominos7 <Rominos7@gmail.com>
2020-11-09 13:54:30 +0200

    basic work with redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,100 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { combineReducers, createStore } from 'redux';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//let initState = document.getElementById('counter').innerText;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const reducer = (state = 0, action)=>{
+       switch (action.type){
+         case 'PLUS':
+          {
+            return state +1;
+          }
+          case 'PLUS_2':
+          {
+            return state +2;
+          }
+          case 'MULTIPLY_2':
+          {
+            return state *2;
+          }
+         case 'MINUS':{
+           return state - 1;
+         }
+         case 'MINUS_2':{
+          return state -2;
+        }
+        case 'DIVIDE_2':{
+          return state /2;
+        }
+        default:{
+          return state;
+        }
+       }  
+};
+
+const reducerBackgrd = (state = 'backgrnON', action)=>{
+   switch(action.type){
+     case 'BACKGRNON':{
+        return 'backgrnON';
+     }
+     case 'BACKGRNOFF':{
+      return 'backgrnOFF';
+     }
+     default:{
+       return state;
+     }
+   }
+}
+
+const GrouprReducers =  combineReducers({
+  reducer,
+  reducerBackgrd
+})
+
+const store = createStore(GrouprReducers);
+
+let flagBGND = 0;
+
+document.getElementById('plus').addEventListener("click",()=>{
+  store.dispatch({type: 'PLUS'});
+  //console.log('store',store.getState().reducer);
+})
+document.getElementById('plus_2').addEventListener("click",()=>{
+  store.dispatch({type: 'PLUS_2'});
+})
+document.getElementById('multiply_2').addEventListener('click',()=>{
+  store.dispatch({type: 'MULTIPLY_2'});
+})
+
+document.getElementById('minus').addEventListener('click',()=>{
+  store.dispatch({type:'MINUS'});
+})
+document.getElementById('minus_2').addEventListener('click',()=>{
+  store.dispatch({type:'MINUS_2'});
+})
+document.getElementById('divide_2').addEventListener('click',()=>{
+  store.dispatch({type:'DIVIDE_2'});
+})
+
+document.getElementById('sw_backgrn').addEventListener("click",()=>{
+  if(flagBGND===0){
+    document.getElementById('backgnd-container').classList.remove(store.getState().reducerBackgrd);
+    store.dispatch({type:'BACKGRNOFF'})
+    flagBGND=1;
+    console.log('flagBNG',flagBGND)
+  }else if(flagBGND===1){
+    document.getElementById('backgnd-container').classList.remove(store.getState().reducerBackgrd);
+    store.dispatch({type:'BACKGRNON'})
+    flagBGND=0;
+    console.log('flagBNG',flagBGND)
+  }
+})
+
+const update =()=>{
+ document.getElementById('counter').innerText = store.getState().reducer;
+ document.getElementById('backgnd-container').classList.add(store.getState().reducerBackgrd);
+};
+
+store.subscribe(update);
+
+
